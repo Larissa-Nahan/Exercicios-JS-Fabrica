c97a577c06e23d3ba192e62bc5444b246d5f5fb7
Larissa-Nahan <larissanahan@gmail.com>
2022-03-24 16:19:12 -0300

    Primeira questão

---
+++ Listas/script.js
@@ -0,0 +1,9 @@
+// 1. Faça um Programa que leia um vetor de 5 números
+
+let minhaLista = [1, 5, 3, 2, 10];
+
+for (let i = 0; i < minhaLista.length; i++) {
+    console.log(minhaLista[i]);
+}
+
+// ----------------------------------------
