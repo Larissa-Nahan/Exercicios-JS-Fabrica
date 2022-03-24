c896480ca295e148dca63fb91796589273bcd5c3
Larissa-Nahan <larissanahan@gmail.com>
2022-03-24 15:31:00 -0300

    Segunda questão

--- EstruturasRepeticao/script.js
+++ EstruturasRepeticao/script.js
@@ -12,3 +12,15 @@ while (true) {
 }
 
 //--------------------------------
+
+// 3. Faça um programa que leia um nome de usuário e a sua senha e não aceite a senha igual ao nome do usuário, mostrando uma mensagem de erro e voltando a pedir as informações.
+
+do {
+  var nome = prompt("Digite seu nome");
+  var senha = prompt("Digite uma senha");
+
+  if (nome == senha) {
+    alert("Nome e senha devem ser distintos");
+  }
+} while (nome == senha);
+
