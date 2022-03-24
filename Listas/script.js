f60dbe197b2e409ac322dd8f7404ca3d487156d5
Larissa-Nahan <larissanahan@gmail.com>
2022-03-24 16:30:59 -0300

    Segunda questão

--- Listas/script.js
+++ Listas/script.js
@@ -7,3 +7,16 @@ for (let i = 0; i < minhaLista.length; i++) {
 }
 
 // ----------------------------------------
+
+// 2. Faça um Programa que leia um vetor de 10 números reais e mostre-os na ordem inversa.
+
+// 2. Faça um Programa que leia um vetor de 10 números reais e mostre-os na ordem inversa.
+
+var numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var inverso = [];
+
+for (let i = numeros.length - 1; i >= 0; i--) {
+  inverso.push(numeros[i]);
+}
+
+console.log(inverso);
